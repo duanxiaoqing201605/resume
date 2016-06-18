044dab6e4f8920f13592bddb451da877bfa54f9d
duanxiaoqing201605 <duanxiaoqing619@163.com>
2016-06-18 21:59:25 +0800

    first

---
+++ resume/js/index.js
@@ -0,0 +1,52 @@
+~function (desW) {
+    var winW = document.documentElement.clientWidth;
+    document.documentElement.style.fontSize = winW / desW * 100 + "px";
+}(750);
+
+~function(){
+    var step = 0,divList=null;
+    var swp=new Swiper(".swiper-container",{
+        loop:true,
+        direction:"vertical",
+        onSlidePrevEnd: function () {
+            step--;
+            change();
+            if (step === 0) {
+                step = 6;
+            }
+        },
+        onSlideNextEnd: function () {
+            step++;
+            change();
+            if (step === 7) {
+                step = 1;
+            }
+        }
+    });
+    function change(){
+        divList=document.querySelectorAll(".swiper-slide");
+        [].forEach.call(divList,function(curDiv,index){
+            curDiv.id=index===step?curDiv.getAttribute("trueId"):null;
+        })
+    }
+}();
+
+~function(){
+    var audioBox=document.querySelector(".audio"),myAudio=audioBox.getElementsByTagName("audio")[0];
+    window.setTimeout(function(){
+        myAudio.play();
+        myAudio.addEventListener("canplay",function(){
+            audioBox.style.display="block";
+            audioBox.className+=" audioMove";
+        },false);
+    },1000);
+    audioBox.addEventListener("click",function(){
+        if(myAudio.paused){
+            myAudio.play();
+            audioBox.className="audio audioMove";
+            return;
+        }
+        myAudio.pause();
+        audioBox.className="audio";
+    },false);
+}();
